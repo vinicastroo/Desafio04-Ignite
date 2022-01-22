8737dad878ca884664b1623259e2b2f70bc5e524
Vinicius Castro da Costa <48870801+vinicastroo@users.noreply.github.com>
2022-01-22 13:52:02 -0300

    [WEB] Migração de Componentes JS -> Componentes TSX

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,23 @@ import {
   useRef,
   useState,
   useCallback,
+  FunctionComponent,
+  InputHTMLAttributes,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props {
+  name: string;
+  icon?: FunctionComponent<any>;
+}
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & Props
+
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +58,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
